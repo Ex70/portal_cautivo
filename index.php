b46db3a550ed21fd0ecaae86dfc231d7930b28ae
Alan Paul Cortés Melchor <sofker07@gmail.com>
2024-11-26 15:18:32 -0600

    otro cambio de nombre

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ require_once("config/conexion.php");
     ?>
 
     <div class="container">
-        <h1>´Portal Cautivo UEH</h1>
+        <h1>Ya dejen de ponernos tanto proyecto</h1>
         <div class="col-md-8">
             <h2>Información Personal</h2>
             <p>Por favor complete los siguientes datos para continuar con el registro.</p>
