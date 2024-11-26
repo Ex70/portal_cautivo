8bbfbd753f1da1de064e9ca74d8a2061a28bb9c9
saandoni <e.com>
2024-11-26 14:50:53 -0600

    Proyecto Inicial

---
+++ view/navbar.php
@@ -0,0 +1,25 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+
+
+
+<!-- Navbar -->
+<nav class="navbar">
+    <a class="navbar-brand" href="#">
+        <!-- <img src="../img/logo-header-ueh.jpg" alt=""> -->
+    </a>
+    <ul class="nav justify-content-end">
+        <li class="nav-item">
+            <a class="nav-link active" href="#">Active</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link disabled" href="#">Disabled</a>
+        </li>
+    </ul>
+</nav>
